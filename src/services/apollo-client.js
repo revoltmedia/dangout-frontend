2518d5405ffbc0bd9cf424b84b425d8d27407042
Josh F <go@revoltmedia.com>
2020-02-12 10:37:32 -0800

    add .env sample and move backend URI to env variable

--- src/services/apollo-client.js
+++ src/services/apollo-client.js
@@ -5,7 +5,7 @@ import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql'
+  uri: process.env.BACKEND_URI
 })
 
 const authLink = setContext((_, { headers }) => {
